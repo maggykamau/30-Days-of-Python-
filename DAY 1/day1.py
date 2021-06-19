d7754109c2df6a0f19956e7703e5f89c4ef741e5
Magnonym0us <shikieshiku@gmail.com>
2021-06-19 10:23:07 +0300

    Day 1 of python commit

--- DAY 1/day1.py
+++ DAY 1/day1.py
@@ -1,1 +1,34 @@
-print("hello") world")
+# DAY 1 OF 30 DAYS CHALLENGE
+#My python version is 3.8.1
+
+print(3+4)   #addition(+)
+print(3-4)   #subtraction(-)
+print(3*4)   #multiplication(*)
+print(4%3)   #modulus(%)
+print(4/3)   #division(/)
+print(3**4)  #exponential(**)
+print(4//3)  #floor division operator(//)
+
+print("***************************************")
+
+name = input("What is your name:")
+family_name =input("what's your family name: ")
+country=input("What is your country name?")
+
+print("My name is " + name)
+print("My family name is " + family_name)
+
+print("My country is " + country)
+
+print("I am enjoying 30 days of python")
+
+print("#######################################################")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type(name))
+print(type(family_name))
+print(type(country))
+
